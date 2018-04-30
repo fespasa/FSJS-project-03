7b83d20710b284154406ba474898e710f4d22d4b
Ferran Espasa <fespasa@me.com>
2018-04-30 17:22:49 +0200

    Activities Section Finished
    All is working fine. Correctly.

--- js/main.js
+++ js/main.js
@@ -5,7 +5,10 @@ window.onload = function() {
 	const shirtSize = document.querySelector('#size');
 	const shirtDesign = document.querySelector('#design');
 	const shirtColor = document.querySelector('#color');
-	console.log(shirtDesign.childNodes);
+	const activities = document.querySelector('.activities');
+	const activitiesInputs = document.querySelectorAll('.activities input');
+	
+	let total = 0;
 	
 	nameField.focus();
 	
@@ -41,4 +44,85 @@ window.onload = function() {
 			}
 		}
 	});
+	
+	/* ---------- ACTIVITIES SECTION ---------- */
+	for(let i = 0; i < activitiesInputs.length; i++) {
+		// Add and event listener to a checkbox state change	
+		activitiesInputs[i].addEventListener("change", () => {
+			addAmount(i); // Call the function that adds the correct ammount to the total
+			isActivityAvaliable(i); // Call a function that check other activities availability
+			console.log(total);
+			// checks if there is a previous ammount on the screen and deletes it
+			if(activities.childNodes.length > 16){
+				activities.removeChild(activities.childNodes[16]);
+			}
+			// creates a new ammount and we add to the DOM
+			if(total !== 0) {
+				let totalAmmount = document.createElement("H3");
+				totalAmmount.innerHTML = "Total: $" + total;
+				activities.appendChild(totalAmmount);
+			}
+		});
+	}
+	
+	function addAmount(index){
+		 
+		if(index === 0) {
+			// if it's the first activity the function adds 200 when is checked. If it's not checked the function substract 200.
+			if(activitiesInputs[0].checked){
+				total = total + 200;
+			} else {
+				total = total - 200;
+			}
+		} else {
+			// if it's any other activity the function adds 100 when is checked. If it's not checked the function substract 100.
+			if(activitiesInputs[index].checked){
+				total = total + 100;
+			} else {
+				total = total - 100;
+			}
+		}
+	}
+	
+	function isActivityAvaliable(index){
+		/* I've created this function that is comproving which activities are available after one of them has been checked. In every case, it calls a function called disableEnableActivity which will enable or disable activities when it's necessary. */
+		switch(index) {
+			case 1:
+				disableEnableActivity(3);
+				disableEnableActivity(5);
+				break;
+			case 2:
+				disableEnableActivity(4);
+				disableEnableActivity(6);
+				break;
+			case 3:
+				disableEnableActivity(1);
+				disableEnableActivity(5);
+				break;
+			case 4:
+				disableEnableActivity(2);
+				disableEnableActivity(6);
+				break;
+			case 5:
+				disableEnableActivity(1);
+				disableEnableActivity(3);
+				break;
+			case 6:
+				disableEnableActivity(2);
+				disableEnableActivity(4);
+				break;
+		}
+		
+		function disableEnableActivity(activityIndex){
+			let activityLabel = activitiesInputs[activityIndex].parentNode;
+			// the function enables the activity and the checkbox if it's disabled and does the opposite when is enabled.
+			if(activitiesInputs[activityIndex].hasAttribute("disabled")){
+				activitiesInputs[activityIndex].removeAttribute("disabled");
+				activityLabel.removeAttribute("style");
+			} else {
+				activitiesInputs[activityIndex].setAttribute("disabled", true);
+				activityLabel.setAttribute("style", "text-decoration: line-through; opacity: 0.6;");
+			}
+		}
+	}
 }
